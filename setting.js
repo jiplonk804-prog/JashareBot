62d5a3958be0afe17abaedb0b6c2b6c24aeef86e
jiplonk804-prog <jiplonk804@gmail.com>
2026-01-08 03:46:43 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,13 +10,13 @@
 ⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞⏤͟͟͞͞
 */
 module.exports = {
-  TOKEN: '8483629717:AAFrvhrId0WOlKvaTGrlkUY32YpBVnH7ygs',
-  OWNER_ID: 8268185735, // owner 
+  TOKEN: '8270678869:AAH1i0rk9wBpgE9IP2iAXf3v1aZ9sAkqsCc,
+  OWNER_ID: 8091720389, // owner 
   GROUP : -1003058450217, // gc 
-  CHANEL : -1003045115977, // chanel lu yaa
-  gclog : -1003097529998, // group buat share all log bot/ history
+  CHANEL : -1003486863300, // chanel lu yaa
+  gclog : -1003449326002, // group buat share all log bot/ history
   MAX_LIMIT: 100,
   TokenVercel : "P6JQ6CrS37J1ONjxlqN9Esf8",
-  MENU_IMAGE_URL: 'https://files.catbox.moe/zzx0s5.jpg', // link foto menu
+  MENU_IMAGE_URL: 'https://files.catbox.moe/kyi8p1.jpg', // link foto menu
 };
 
